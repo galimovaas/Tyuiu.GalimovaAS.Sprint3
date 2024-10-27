cfe8ddcfd57e25ba5ce22c0e5728dc9f9bf41a94
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-10-27 21:56:05 +0500

    Task3

--- Tyuiu.GalimovaAS.Sprint3.Task0.V22.Lib/DataService.cs
+++ Tyuiu.GalimovaAS.Sprint3.Task0.V22.Lib/DataService.cs
@@ -5,11 +5,11 @@ namespace Tyuiu.GalimovaAS.Sprint3.Task0.V22.Lib
     {
         public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
-            double multSeries = 0;
+            double multSeries = 1;
             int i;
             for (i = startValue; i <= stopValue; i++)
             {
-                multSeries = multSeries + ((Math.Pow(value, i) + 4) * Math.Cos(value));
+                multSeries = multSeries * ((Math.Pow(value, i) + 4) * Math.Cos(value));
             }
             return Math.Round(multSeries, 3);
         }
