c927c1f31a011cfaf10037e8663326a0031d8372
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-10-30 20:40:49 +0500

    Task5

---
+++ Tyuiu.GalimovaAS.Sprint3.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.GalimovaAS.Sprint3.Task5.V5.Lib
+{
+    public class DataService : ISprint3Task5V5
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeries = sumSeries + (x / Math.Sin(j));
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.GalimovaAS.Sprint3.Task5.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.GalimovaAS.Sprint3.Task5.V5.Lib;
+namespace Tyuiu.GalimovaAS.Sprint3.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Галимова А.С. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                                    *");
+            Console.WriteLine("* Задание #5                                                                               *");
+            Console.WriteLine("* Вариант #5                                                                               *");
+            Console.WriteLine("* Выполнила: Выполнила: Галимова А.С. | АСОиУБ - 24-1                                      *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму ряда по формуле, при X=5.                    *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            Console.WriteLine("Переменная x = ");
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue2);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
