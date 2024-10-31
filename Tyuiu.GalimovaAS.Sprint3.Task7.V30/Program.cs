ec74a9acc20923bcc523f04f5031f3530c6eb8fa
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-11-01 01:05:38 +0500

    Task7

---
+++ Tyuiu.GalimovaAS.Sprint3.Task7.V30/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.GalimovaAS.Sprint3.Task7.V30.Lib;
+namespace Tyuiu.GalimovaAS.Sprint3.Task7.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Галимова А.С. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                                  *");
+            Console.WriteLine("* Задание #7                                                                               *");
+            Console.WriteLine("* Вариант #30                                                                             *");
+            Console.WriteLine("* Выполнила: Галимова А.С. | АСОиУБ - 24-1                                                 *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции                             *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
